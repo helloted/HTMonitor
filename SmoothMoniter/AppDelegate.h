b4b9c8d9e57f2fa3535a8a43fd141fcb97a1487a
曹浩之 <ios_dev_0@devondtech.com>
2017-04-18 10:51:42 +0800

    Initial Commit

---
+++ SmoothMoniter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmoothMoniter
+//
+//  Created by iMac0 on 2017/4/18.
+//  Copyright © 2017年 iMac0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmoothMoniter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SmoothMoniter
+//
+//  Created by iMac0 on 2017/4/18.
+//  Copyright © 2017年 iMac0. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
